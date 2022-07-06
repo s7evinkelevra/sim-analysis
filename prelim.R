ae6265fd4d600f6ba453a3dacacfd2b9c097ddd8
s7evinkelevra <kelevra.1337@gmail.com>
2022-07-06 20:09:18 +0200

    add some analyses

--- prelim.R
+++ prelim.R
@@ -6,12 +6,6 @@ library(tidyverse)
 
 library(esquisse)
 
-data_dir = "./data"
-current_run_id = "run-2022-06-22-1"
-current_run_path = file.path(data_dir, current_run_id)
-
-output_dir = "./output"
-
 # nice name lol
 read_config_config <- function(config_path) {
   return(rjson::fromJSON(file=file.path(config_path, "config.json")))
@@ -90,16 +84,30 @@ property_from_config_id <- function(config_id, property_name){
   return(run_config_summary[config_id, property_name])
 }
 
+add_data <- function(base, adding){
+  return(left_join(base, adding, by = c("config_id", "generation")))
+}
+
+
+data_dir <- "./data"
+current_run_id <- "2022-07-04-1"
+current_run_path <- file.path(data_dir, current_run_id)
+
+output_dir <- "./output"
+
 
 # Read run data
 run_configs = read_run_configs(current_run_path)
 
-
+# read allele frequency data
 run_host_allele_data = read_run_data(current_run_path, "host_allele_data.csv")
 run_host_allele_data_combined = bind_rows(run_host_allele_data, .id = "config_id")
+
+# read run meta data
 run_meta_data = read_run_data(current_run_path, "meta_data.csv")
 run_meta_data_combined = bind_rows(run_meta_data, .id="config_id")
 
+# build mode info from flags
 run_meta_data_combined = run_meta_data_combined %>% 
   mutate(bInitialMode =
     bInfection == 0 & 
@@ -129,10 +137,7 @@ run_meta_data_combined = run_meta_data_combined %>%
     bPathogenFitnessproportionalReproduction == 0 &
     bPathogenMutation == 1 &
     bHostMutation == 1
-  )
-
-
-
+)
 
 
 # Create output folder for this run
@@ -148,15 +153,72 @@ run_config_changed_mask = run_config_summary %>% sapply(function(x) !length(uniq
 run_config_changed = run_config_summary[, run_config_changed_mask]
 run_config_common = run_config_summary[1, !run_config_changed_mask]
 
+# write common config values in long format to file for better use later
 run_config_key_value = run_config_common %>% pivot_longer(cols = everything(), names_to = "property", values_to = "values")
 write.csv(run_config_key_value, file.path(output_dir, current_run_id, "config_common.csv"), row.names = FALSE)
 
-
+# get allele counts by config, generation and species
 run_allele_counts = run_host_allele_data_combined %>% count(config_id, generation, species)
 run_allele_counts_grouped = run_allele_counts %>% group_by(config_id)
 
+# add meta data to allele counts -> simulation mode most importantly
+run_allele_counts_grouped_meta = add_data(run_allele_counts_grouped, run_meta_data_combined)
 
 
+# select the last 100 burnin and last 100 post-burnin generations
+run_allele_counts_last_100 <- run_allele_counts_grouped_meta %>%
+  group_by(config_id, bBurninMode) %>%
+  filter(generation >= max(generation) - 100)
+
+
+# build mean allele counts and SD by config and mode for the last 100 generations
+run_allele_counts_last_100_mean <- run_allele_counts_last_100 %>%
+  summarise(mean_alleles = mean(n), sd = sd(n), .groups = "keep")
+
+
+
+## Allele frequency distribution
+run_host_allele_data_combined_meta <- add_data(run_host_allele_data_combined, run_meta_data_combined)
+
+
+run_allele_data_first_last_1 <- run_host_allele_data_combined_meta %>%
+  group_by(config_id, bBurninMode) %>%
+  filter(generation == max(generation) | generation == 0)
+
+summary(run_allele_data_first_last_1)
+
+
+allele_dist_first_last_plt = ggplot(run_allele_data_first_last_1) +
+  aes(
+    x = count,
+    facet = generation,
+    group = bBurninMode
+  ) +
+  geom_histogram( color="#e9ecef", alpha=0.6, binwidth = 5) +
+  facet_wrap(
+    vars(generation),
+  )
+
+allele_dist_first_last_plt
+
+
+# boxplot of the allele counts of the last 100 generations by simulation mode
+run_allele_counts_last_100_box_plt = ggplot(run_allele_counts_last_100) +
+  aes(
+    x = bBurninMode,
+    y = n
+  ) +
+  geom_boxplot() +
+  geom_jitter(width = 0.2, aes(color = config_id)) +
+  theme_minimal() +
+  scale_y_continuous(breaks = seq(0, 1000, by = 10))
+
+run_allele_counts_last_100_box_plt
+
+ggsave(file.path(output_dir, current_run_id, "allele_counts_box.png"), plot = run_allele_counts_last_100_box_plt, width = 3000, height = 3000, units = "px", bg = "#ffffff")
+
+
+# allele counts over time
 run_allele_counts_plt = ggplot(run_allele_counts_grouped) +
   aes(
     x = generation,
@@ -175,7 +237,7 @@ run_allele_counts_plt = ggplot(run_allele_counts_grouped) +
 
 
 run_allele_counts_plt
-ggsave(file.path(output_dir, current_run_id, "allele_counts.png"), plot = run_allele_counts_plt, width = 5000, height = 5000, units = "px")
+ggsave(file.path(output_dir, current_run_id, "allele_counts.png"), plot = run_allele_counts_plt, width = 5000, height = 5000, units = "px", bg = "#ffffff")
 
 # run_allele_frequencies_plt = ggplot(run_host_allele_data_combined) +
 #   aes(
